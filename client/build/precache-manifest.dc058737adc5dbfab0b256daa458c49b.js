e96ce6927c0a255cff18bb0c43e2dd6daab3360c
sidsheng <sidsheng@gmail.com>
2020-06-24 08:13:13 +1000

    moved gitignore back into right location

---
+++ client/build/precache-manifest.dc058737adc5dbfab0b256daa458c49b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d334338b6f01e8be14291130c7a507bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af808aaad27c5f8cf39f",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "743cb3f389247e8c735d",
+    "url": "/static/js/2.b4006025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4006025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af808aaad27c5f8cf39f",
+    "url": "/static/js/main.4a70b384.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
